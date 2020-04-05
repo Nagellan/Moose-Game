f8f0e49eeb7aa19a0eafba515baae077f2a849bd
Nagellan <31626879+Nagellan@users.noreply.github.com>
2020-04-05 16:21:31 +0300

    Move score from Moose to Tournament * Fix bug w/ not scoring up on the first move.

--- src/Moose.java
+++ src/Moose.java
@@ -2,7 +2,6 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class Moose extends Player {
-    private double score;
     private LinkedList<Integer> moves;
     private LinkedList<Integer> opponentMoves;
 
@@ -12,7 +11,6 @@ public class Moose extends Player {
 
     @Override
     public void reset() {
-        this.score = 0;
         this.moves = new LinkedList<>();
         this.opponentMoves = new LinkedList<>();
     }
@@ -20,41 +18,18 @@ public class Moose extends Player {
     @Override
     public int move(int opponentLastMove, int xA, int xB, int xC) {
         int move;
-        int lastMove;
-        int[] fields = {xA, xB, xC};
 
-        // make the first turn into the random field without saving opponent's move
-        if (opponentLastMove == 0) {
-            move = new Random().nextInt(3) + 1;
-            this.moves.push(move);
-            return move;
-        }
+        // make move (first one is random)
+        move = opponentLastMove == 0 ? new Random().nextInt(3) + 1 : this.decide();
 
-        this.opponentMoves.push(opponentLastMove);
-        lastMove = this.moves.peek();
-
-        // if both moose are in one field, reset score and don't eat
-        if (opponentLastMove == lastMove) {
-            this.score = 0;
-        } else {
-            this.eat(fields[lastMove - 1]);
-        }
-
-        move = this.decide();
+        // save moves
         this.moves.push(move);
+        this.opponentMoves.push(opponentLastMove);
 
         return move;
     }
 
-    private void eat(int x) {
-        this.score += 10 * Math.pow(Math.E, x) / (1 + Math.pow(Math.E, x)) - 5;
-    }
-
     private int decide() {
         return new Random().nextInt(3) + 1;
-    }
-
-    public double getScore() {
-        return score;
     }
 }
--- src/Tournament.java
+++ src/Tournament.java
@@ -2,33 +2,52 @@ public class Tournament {
     private int xA;
     private int xB;
     private int xC;
+
     private int numRounds;
+
     private Moose moose1;
     private Moose moose2;
+    private double moose1Score;
+    private double moose2Score;
 
     public Tournament(Moose moose1, Moose moose2) {
         this.xA = 1;
         this.xB = 1;
         this.xC = 1;
+
         this.numRounds = 0;
+
         this.moose1 = moose1;
         this.moose2 = moose2;
+
+        this.moose1Score = 0;
+        this.moose2Score = 0;
     }
 
     public void play(int numRounds) {
         this.numRounds = numRounds;
+
         int[] fieldsChange = {1, 1, 1};
 
         int moose1LastMove = 0;
         int moose2LastMove = 0;
 
         for (int r = 0; r < numRounds; r++) {
+            int[] fields = {this.xA, this.xB, this.xC};
+
+            // make moves
             int moose1Move = this.moose1.move(moose2LastMove, this.xA, this.xB, this.xC);
             int moose2Move = this.moose2.move(moose1LastMove, this.xA, this.xB, this.xC);
 
+            // update moose scores
+            this.moose1Score = moose1Move == moose2Move ? 0 : this.getScore(fields[moose1Move - 1]);
+            this.moose2Score = moose1Move == moose2Move ? 0 : this.getScore(fields[moose2Move - 1]);
+
+            // calculate changes in fields' stats
             fieldsChange[moose1Move - 1] = -1;
             fieldsChange[moose2Move - 1] = -1;
 
+            // update fields stats
             this.xA = Math.max(0, this.xA + fieldsChange[0]);
             this.xB = Math.max(0, this.xB + fieldsChange[1]);
             this.xC = Math.max(0, this.xC + fieldsChange[2]);
@@ -36,12 +55,20 @@ public class Tournament {
             moose1LastMove = moose1Move;
             moose2LastMove = moose2Move;
         }
+
+        // reset both moose memory
+        this.moose1.reset();
+        this.moose2.reset();
     }
 
     public void getResults() {
         System.out.println("Amount of rounds: " + this.numRounds);
         System.out.println("Fields stats: xA=" + this.xA + ", xB=" + this.xB + ", xC=" + xC);
-        System.out.println("Moose 1 score: " + this.moose1.getScore());
-        System.out.println("Moose 2 score: " + this.moose2.getScore());
+        System.out.println("Moose 1 score: " + this.moose1Score);
+        System.out.println("Moose 2 score: " + this.moose2Score);
+    }
+
+    private double getScore(int x) {
+        return 10 * Math.pow(Math.E, x) / (1 + Math.pow(Math.E, x)) - 5;
     }
 }
